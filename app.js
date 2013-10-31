ec2381fd3d40f7daa31951df1cd42391cb1f263a
Pasi Jokinen <pasi@workflower.fi>
2013-10-31 15:48:21 +0200

    Animate on page load

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var $ = require('jquery-browserify'),
 
 $d.ready(function(){
     a           = false;
-    $p          = $('#path');
+    $p          = $('path');
     p           = $p[0];
     pathLength  = p.getTotalLength();
     pageHeight  = document.body.clientHeight - window.innerHeight;
@@ -21,12 +21,26 @@ $d.ready(function(){
       // set its attributes
       a.setAttributeNS(null, 'attributeName', 'stroke-dashoffset');
       a.setAttributeNS(null, 'dur', 1);
-      a.setAttributeNS(null, 'to', 0);
+      a.setAttributeNS(null, 'to', pathLength);
       a.setAttributeNS(null, 'from', pathLength);
       a.setAttributeNS(null, 'fill', 'freeze');
 
       // link the a to the target
     p.appendChild(a);
+
+    $('#path2').attr('stroke-dashArray', [pathLength/3,  pathLength/3*5])
+               .attr('stroke-dashoffset', pathLength/3);
+
+    var b = document.createElementNS(
+          'http://www.w3.org/2000/svg', 'animate');
+      // set its attributes
+      b.setAttributeNS(null, 'attributeName', 'stroke-dashoffset');
+      b.setAttributeNS(null, 'dur', 4);
+      b.setAttributeNS(null, 'to',   -pathLength/3*5);
+      b.setAttributeNS(null, 'from', pathLength/3);
+      b.setAttributeNS(null, 'fill', 'freeze');
+
+    $('#path2')[0].appendChild(b);
 
     $(window).scroll( onScroll );
 });
@@ -45,3 +59,11 @@ function onScroll(event){
 
     prevOffset = offset;
 }
+
+/*
+ * Return outerHTML for the first element in a jQuery object,
+ * or an empty string if the jQuery object is empty;
+ */
+jQuery.fn.outerHTML = function() {
+   return (this[0]) ? this[0].outerHTML : '';
+};
